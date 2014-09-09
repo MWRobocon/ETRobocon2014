1c03a03535a0a29cf99c36fb133268f109939792
dsethi <danvirsingh@gmail.com>
2014-09-09 18:24:45 -0400

    Adding CV Team minions detection

--- models/host/CVTeam/MultiObjectTracking/matchWithTemplate.m
+++ models/host/CVTeam/MultiObjectTracking/matchWithTemplate.m
@@ -1,23 +1,19 @@
-function objectRegion = matchWithTemplate(scene, template)
-
+function [objectRegion NMatchedPoints] = matchWithTemplate(scene, template)
 %% Get features from template image
 
 bwTemplate = rgb2gray(template);
 scene = rgb2gray(scene);
-% Region of interest = [xmin ymin width height]. hard coded for each image
-% ROI = [200 130 210 200]; %For LomographyTemplate.jpg
-% ROI = [84   139   480   205]; %For Gautam.jpg
-% ROI = [244 181 106 130]; %For Gautam.jpg 640 x 480
+% Template region of interest = [xmin ymin width height]
 ROI = [0 0 size(template,1) size(template,2)];
 % imshow(template);
 % disp('Select rectangular ROI for Template');
 % ROI = round(getPosition(imrect));
 
 pointsTemplate = detectSURFFeatures(bwTemplate); %, 'ROI', ROI);
-imshow(template);
-hold on;
-plot(pointsTemplate);
-title('Template image with detected features');
+% imshow(template);
+% hold on;
+% plot(pointsTemplate);
+% title('Template image with detected features');
 
 % Get objectRegion from features and RegionOfInterest of template
 scenePoints = detectSURFFeatures(scene);
@@ -27,15 +23,17 @@ boxPoints = pointsTemplate; %converting to variables of demo
 [boxFeatures, boxPoints] = extractFeatures(bwTemplate, boxPoints);
 [sceneFeatures, scenePoints] = extractFeatures(scene, scenePoints);
 boxPairs = matchFeatures(boxFeatures, sceneFeatures);
+NMatchedPoints = size(boxPairs,1);
 
 matchedBoxPoints = boxPoints(boxPairs(:, 1), :);
 matchedScenePoints = scenePoints(boxPairs(:, 2), :);
 [tform, inlierBoxPoints, inlierScenePoints] = estimateGeometricTransform(matchedBoxPoints, matchedScenePoints, 'affine');
 
-figure;
-showMatchedFeatures(bwTemplate, scene, inlierBoxPoints, inlierScenePoints, 'montage');
-title('Matched Points (Inliers Only)');
+% figure;
+% showMatchedFeatures(bwTemplate, scene, inlierBoxPoints, inlierScenePoints, 'montage');
+% title('Matched Points (Inliers Only)');
 
+% Convert ROI params into corners of a box
 boxPolygon = [ROI(1), ROI(2); ... %top-left
     ROI(1)+ ROI(3), ROI(2); ... %top-right
     ROI(1)+ ROI(3), ROI(2)+ ROI(4); ... %bottom right
@@ -43,10 +41,10 @@ boxPolygon = [ROI(1), ROI(2); ... %top-left
     ROI(1), ROI(2)]; %top-left again to close the loop
 
 newBoxPolygon = transformPointsForward(tform, boxPolygon);
-figure; imshow(scene);
-hold on;
-line(newBoxPolygon(:, 1), newBoxPolygon(:, 2), 'Color', 'y');
-title('Detected Template');
+% figure; imshow(scene);
+% hold on;
+% line(newBoxPolygon(:, 1), newBoxPolygon(:, 2), 'Color', 'y');
+% title('Detected Template');
 xmax = max(newBoxPolygon(:,1));
 ymax = max(newBoxPolygon(:,2));
 xmin = min(newBoxPolygon(:,1));
