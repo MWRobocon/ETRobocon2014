3404ad7409a14ec2c53b042100069917db2e32be
gatinc <gokhanatinc@gmail.com>
2014-09-14 15:18:10 -0400

    Up & Down working BT status sent for top of mountain & down the ramp

--- data_types/enumerations/RobotID.m
+++ data_types/enumerations/RobotID.m
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-classdef  ( ...
-        Hidden = false,          ... % = [false]|true
-        InferiorClasses = {},    ... % = {}
-        ConstructOnLoad = false, ... % = [false]|true
-        Sealed = false           ... % = [false]|true
-        ) ...
-        RobotID < Simulink.IntEnumType
-    % RobotID: This enumeration is used to indicate the ID of
-    % robots available in the ET Robocon Architect performance.
-    
-    enumeration
-        NXT1           ( 1 )
-        NXT2           ( 2 )
-        ARDUINO        ( 3 )
-        LEDCUBE       ( 4 )
-    end
-    
-=======
 classdef  ( ...
         Hidden = false,          ... % = [false]|true
         InferiorClasses = {},    ... % = {}
@@ -34,7 +15,6 @@ classdef  ( ...
         LEDCUBE       ( 4 )
         NXT_BEN       ( 5 );
         ZUMO_ADAFRUIT_32B0 ( 6 );
+        NXT (7);
     end
-    
->>>>>>> refs/remotes/origin/Comms
 end
